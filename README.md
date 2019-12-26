173fe7792473e99ec75d6f642a78372eb167bde3
ino_h <syslink.h.inoue@gmail.com>
2019-12-26 21:00:34 +0900

    Heroku に合わせてデータベースを postgresql に変更

--- README.md
+++ README.md
@@ -5,7 +5,10 @@
 - Python のインストール
 - pip
   - Django のインストール
-  - mysqlclient のインストール
+  - mysql の場合
+    - mysqlclient のインストール
+  - postgreslq の場合
+    - psycopg2-binary のインストール
 
 ## Usage
 
--- service_api/settings.py
+++ service_api/settings.py
@@ -74,14 +74,25 @@ WSGI_APPLICATION = 'service_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.mysql',
+#          'NAME': 'service_api_development',
+#          'USER': 'root',
+#          'PASSWORD': 'password',
+#          'HOST': '127.0.0.1',
+#          'PORT': '3306'
+#      }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'service_api_development',
-        'USER': 'root',
+        'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'PORT': '5432'
     }
 }
 
